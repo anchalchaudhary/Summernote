610042c39fc9e923ba93851eadd9e4075f163102
Anchal Chaudhary <anchal.ldn5@gmail.com>
2017-04-12 11:41:49 +0530

    Formatted Text displayed

---
+++ ViewText.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class ViewText : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string text = "";
+        string CS = ConfigurationManager.ConnectionStrings["SummernoteCS"].ConnectionString;
+        using (SqlConnection connect = new SqlConnection(CS))
+        {
+            using (SqlCommand cmd = new SqlCommand("Select Text from tblTypedText"))
+            {
+                cmd.Connection = connect;
+                connect.Open();
+                text = cmd.ExecuteScalar().ToString();
+            }
+        }
+
+        lblMyText.Text = System.Web.HttpUtility.HtmlDecode(text);
+    }
+}
